a1617c55e2fe2c96cd27fb3fe451b902a625024c
e-Mangueira <nm.mangueira@ufma.br>
2022-07-16 19:38:35 +0100

    JUnit - teste sem ferramenta 1

---
+++ src/main/java/services/LocationServices.java
@@ -0,0 +1,33 @@
+package services;
+
+import java.util.Date;
+import static utilities.DateUtility.adicionarDias;
+
+import entities.Location;
+import entities.Movie;
+import entities.User;
+
+public class LocationServices {
+	
+	public Location alugarFilme(User usuario, Movie filme) {
+		Location locacao = new Location();
+		locacao.setFilme(filme);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		locacao.setValor(filme.getPrecoLocacao());
+
+		//Entrega no dia seguinte
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+		
+		//Salvando a locacao...	
+		//TODO adicionar método para salvar
+		
+		return locacao;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
